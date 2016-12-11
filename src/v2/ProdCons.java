4c664a85ef40b008605ff4493b4921c33bdd1bb0
nicomy <nicomy_68@hotmail.fr>
2016-12-11 21:34:49 +0100

    v2

---
+++ src/v2/ProdCons.java
@@ -0,0 +1,97 @@
+package v2;
+
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons._Producteur;
+
+public class ProdCons implements Tampon {
+	// nbplein correspond au nombre de ressource dans le buffer.
+	private int nbplein, N ; // N est le nombre maximum de messages que peut contenir le buffer. 
+	 
+	private int in = 0 , out = 0,nbProd = 0 ; 
+	
+	
+	private Message[] buffer ;
+	
+	
+	public ProdCons( int n ){
+		N = n ;
+		nbplein = 0 ;  
+		buffer = new Message[N]; 
+	}
+
+	
+	public synchronized int enAttente() {
+		return nbplein;
+	}
+	
+	// fonction permettant de retirer une ressource dans le tampon. 
+	public synchronized Message get(_Consommateur c) throws Exception, InterruptedException {
+		// tant qu'il n'y a rien a lire le processus attend. 
+		System.out.println("coucou je suis le consomateur "+ ((Consommateur) c).get_id() );
+		Message m;
+		while (nbplein == 0 ) wait() ; 
+		
+		// gestion du buffer
+		synchronized(this){
+			m= buffer[out];
+			out = (out++) % N ;
+		}
+		
+		//on décremente le nombre de ressource dispo
+		nbplein -- ;
+		
+		notifyAll();
+		System.out.println("aurevoir je suis le consomateur "+ ((Consommateur) c).get_id() );
+		return m;
+	}
+	
+	// fonction permettant de dï¿½poser une ressource dans le tampon. 
+	public synchronized void put(_Producteur p, Message m) throws Exception, InterruptedException {
+		//si le buffer est plein on attend : 
+		while(nbplein == N ) wait() ;
+		
+		//mettre ï¿½ jour le buffer 
+//		synchronized(this){
+			buffer[in] = m ;
+			in = (in +1) %N;
+//		}
+		//on incremente le nombre de ressource dispo 
+		nbplein ++ ; 
+		
+		notifyAll();
+	}
+	
+
+	public int taille() {
+		return N;
+	}
+
+	public synchronized void nouveau_prod(){
+		System.out.println("le poducteur "+ nbProd+" rentre dans le game");
+		nbProd++;
+//		System.out.println(nbProd);
+		
+	}
+	public synchronized void fin_prod(){
+		
+		nbProd-- ; 
+		System.out.println("le poducteur "+ nbProd+" sort de la game");
+	}
+	
+	// return vrai si il n'y a plus de pproducteur et que le bufer est vide
+	public synchronized boolean fin() {
+		boolean resultat = ((nbProd == 0) && ( nbplein == 0 ));
+		//System.out.println("resultat fin = "+ resultat);
+		
+		
+		
+		//On s'assure qu'il n'y pas de nouveau producteur crÃ©e. 
+		if(resultat){
+			notifyAll();
+		}
+		
+		return (nbProd == 0) && ( nbplein == 0 );
+	} 
+}
---
+++ src/v2/TestProdCons.java
@@ -0,0 +1,118 @@
+package v2;
+import java.util.ArrayList;
+
+import jus.poc.prodcons.*;
+
+public class TestProdCons extends Simulateur {
+	private Observateur Ob ; 
+	private ProdCons buffer ;
+	private ArrayList<Producteur> lprod ;
+	private ArrayList<Consommateur> lcons ;
+	
+	
+	// liste des options
+	protected static int nbProd;
+	protected static int nbCons;
+	protected static int nbBuffer;
+	protected static int tempsMoyenProduction;
+	protected static int deviationTempsMoyenProduction;
+	protected static int tempsMoyenConsommation;
+	protected static int deviationTempsMoyenConsommation;
+	protected static int nombreMoyenDeProduction;
+	protected static int deviationNombreMoyenDeProduction;
+	protected static int nombreMoyenNbExemplaire;
+	protected static int deviationNombreMoyenNbExemplaire;
+	
+	
+	//On remplie dans cette classe les deux listes lcons et lprod par le nb voulue
+	public TestProdCons(Observateur observateur){
+		super(observateur);
+		Ob = observateur;
+		init("options.xml");
+		
+		buffer = new ProdCons(nbBuffer);
+		
+		//creation des porducteurs
+		lprod = new ArrayList<>();
+		for(int i = 0 ; i< nbProd ; i ++ ){
+			try {
+				lprod.add(new Producteur(i, buffer,
+						Ob,tempsMoyenProduction, deviationNombreMoyenDeProduction,
+						nombreMoyenDeProduction, deviationNombreMoyenDeProduction));
+			} catch (ControlException e) {
+				System.out.println("erreur a la creation de Producteur");
+				e.printStackTrace();
+			}
+			
+		}
+		
+		//mise en place des consomateurs
+		lcons = new ArrayList<>();
+		for(int i = 0 ; i< nbCons ; i ++ ){
+			try {
+				lcons.add(new Consommateur(i,Ob, tempsMoyenConsommation,
+						deviationTempsMoyenConsommation, buffer));
+			} catch (ControlException e) {
+				System.out.println("erreur cr�ation consomateur");
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
+	// On lance chaqu'un des consomateurs dans lcons et producteur dans lprod  
+	protected void run() throws Exception{
+		
+		for (int i=0;i<nbProd;i++){
+//			if (lprod.get(i)==null) System.out.println("on peut pas acceder � un Prdo\n");
+//			else System.out.println(lprod.get(i).toString());
+			lprod.get(i).start();
+		}
+		for (int i=0;i<nbCons;i++){
+//			System.out.println(c.get(i).toString());
+			lcons.get(i).start();
+		}	
+		
+		
+	}	
+	
+//	protected <type> option;
+	
+	/**
+	* Retreave the parameters of the application.
+	* @param file the final name of the file containing the options.
+	*/
+	protected static void init(String file) {
+	// retreave the parameters of the application
+	final class Properties extends java.util.Properties {
+	private static final long serialVersionUID = 1L;
+	public int get(String key){return Integer.parseInt(getProperty(key));}
+		public Properties(String file) {
+			try{
+				loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+			}catch(Exception e){e.printStackTrace();}
+			
+			}
+		}
+		Properties option = new Properties("options/"+file);
+		nbProd = option.get("nbProd");
+		System.out.println("nb_prod =" + nbProd);
+		nbCons = option.get("nbCons");
+		System.out.println("nbCons = "+ nbCons);
+		nbBuffer = option.get("nbBuffer");
+		tempsMoyenProduction = option.get("tempsMoyenProduction");
+		deviationTempsMoyenProduction = option.get("deviationTempsMoyenProduction");
+		tempsMoyenConsommation = option.get("tempsMoyenConsommation");
+		deviationTempsMoyenConsommation = option.get("deviationTempsMoyenConsommation");
+		nombreMoyenDeProduction = option.get("nombreMoyenDeProduction");
+		deviationNombreMoyenDeProduction = option.get("deviationNombreMoyenDeProduction");
+		nombreMoyenNbExemplaire = option.get("nombreMoyenNbExemplaire");
+		deviationNombreMoyenNbExemplaire = option.get("deviationNombreMoyenNbExemplaire");
+	}
+//	
+//	<option> = option.getProperty("option");
+	
+	
+	public static void main(String[] args){new TestProdCons(new Observateur()).start();}
+}
